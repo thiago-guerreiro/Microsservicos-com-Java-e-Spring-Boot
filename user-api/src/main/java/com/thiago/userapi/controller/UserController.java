53a9e6838ad31b205d6e3d493c2e9cfa634943a5
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-09-06 11:06:13 -0300

    Camadas service e controller

---
+++ user-api/src/main/java/com/thiago/userapi/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.thiago.userapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thiago.userapi.dto.UserDTO;
+import com.thiago.userapi.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/users")
+	public List<UserDTO> getUsers() {
+		List<UserDTO> usuarios = userService.getAll();
+		return usuarios;
+	}
+
+	@PostMapping("/user/{id}")
+	public UserDTO findById(@PathVariable Long id) {
+		return userService.findById(id);
+	}
+
+	@PostMapping("/user")
+	public UserDTO newUser(@RequestBody UserDTO userDTO) {
+		return userService.save(userDTO);
+	}
+
+	@GetMapping("/user/cpf/{cpf}")
+	public UserDTO getUsersFiltro(@PathVariable String cpf) {
+		return userService.findByCpf(cpf);
+	}
+
+	@DeleteMapping("/user/{id}")
+	public UserDTO delete(@PathVariable Long id) {
+		return userService.delete(id);
+	}
+
+	@GetMapping("/user/search")
+	public List<UserDTO> queryByName(@RequestParam(name = "nome", required = true) String nome) {
+		return userService.queryByName(nome);
+	}
+
+}
