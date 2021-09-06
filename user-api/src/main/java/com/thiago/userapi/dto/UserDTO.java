c3ed3a80390cd4b7f0ea0081b5bb7f2b2c8bb7be
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-09-06 10:53:27 -0300

    Camadas model, dto e repository

---
+++ user-api/src/main/java/com/thiago/userapi/dto/UserDTO.java
@@ -0,0 +1,75 @@
+package com.thiago.userapi.dto;
+
+import java.util.Date;
+
+import com.thiago.userapi.model.User;
+
+public class UserDTO {
+
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String email;
+	private String telefone;
+	private Date dataCadastro;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public static UserDTO convert(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setNome(user.getNome());
+		userDTO.setEndereco(user.getEndereco());
+		userDTO.setCpf(user.getCpf());
+		userDTO.setEmail(user.getEmail());
+		userDTO.setTelefone(user.getTelefone());
+		userDTO.setDataCadastro(user.getDataCadastro());
+		return userDTO;
+	}
+
+}
---
+++ user-api/src/main/java/com/thiago/userapi/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.thiago.userapi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thiago.userapi.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByCpfAndKey(String cpf);
+
+	List<User> queryByNomeLike(String name);
+
+}
